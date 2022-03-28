bee2d0a1208f417a8196274a55dd28615c624547
rea <m.rakhely.viktoria@gmail.com>
2022-03-28 13:05:56 +0200

    third solution

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+use Rea\FizzBuzz\Solutions\FirstSolution;
+use Rea\FizzBuzz\Solutions\SecondSolution;
+use Rea\FizzBuzz\Solutions\ThirdSolution;
+
+require_once 'vendor/autoload.php';
+
+//echo "First solution:\n";
+//$firstSolution = new FirstSolution();
+//$firstSolution->getFirstSolution();
+
+//echo "Second solution:\n";
+//$secondSolution = new SecondSolution();
+//$secondSolution->getSecondSolution();
+
+echo "Third solution:\n";
+$thirdSolution = new ThirdSolution();
+$thirdSolution->getThirdSolution();
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '8da6d7945dea26626dd1c4c78ae8bb5e85f32a80',
         'name' => 'rea/fizz-buzz',
         'dev' => true,
     ),
     'versions' => array(
         'rea/fizz-buzz' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '8da6d7945dea26626dd1c4c78ae8bb5e85f32a80',
             'dev_requirement' => false,
         ),
     ),
