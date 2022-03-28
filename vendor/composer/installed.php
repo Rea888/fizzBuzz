8da6d7945dea26626dd1c4c78ae8bb5e85f32a80
rea <m.rakhely.viktoria@gmail.com>
2022-03-28 11:29:00 +0200

    first solution

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'rea/fizz-buzz',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rea/fizz-buzz' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
